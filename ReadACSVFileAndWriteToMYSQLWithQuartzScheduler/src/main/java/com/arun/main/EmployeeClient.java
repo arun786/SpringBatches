03dc5d8617441c1c6d0bf17dc5e5ce797e359b62
arun789 <adwiti1975@gmail.com>
2017-01-02 01:02:09 -0700

    removing the comments

--- ReadACSVFileAndWriteToMYSQLWithQuartzScheduler/src/main/java/com/arun/main/EmployeeClient.java
+++ ReadACSVFileAndWriteToMYSQLWithQuartzScheduler/src/main/java/com/arun/main/EmployeeClient.java
@@ -1,34 +1,11 @@
 package com.arun.main;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobExecution;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class EmployeeClient {
 	public static void main(String[] args) {
 		ApplicationContext ctx = new ClassPathXmlApplicationContext("springjob.xml");
-		/*Job job = ctx.getBean("employeeJob",Job.class);
-		JobLauncher launcher = ctx.getBean("jobLauncher",JobLauncher.class);
-		try {
-			JobExecution execution = launcher.run(job, new JobParameters());
-			System.out.println(execution.getStatus());
-		} catch (JobExecutionAlreadyRunningException e) {
-			e.printStackTrace();
-		} catch (JobRestartException e) {
-			e.printStackTrace();
-		} catch (JobInstanceAlreadyCompleteException e) {
-			e.printStackTrace();
-		} catch (JobParametersInvalidException e) {
-			e.printStackTrace();
-		}*/
-		
 	}
 
 }
